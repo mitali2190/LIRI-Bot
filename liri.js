ca6fd21a4d9807e144c839be44e7f3b2a3281d4d
mitali2190 <contractormitali21@gmail.com>
2019-01-29 18:34:50 -0800

    newwork

--- liri.js
+++ liri.js
@@ -1,118 +1,104 @@
 require("dotenv").config();
 var request = require("request");
 var Spotify = require('node-spotify-api');
-var keys = require("./keys.js");
+// var keys = require("./keys.js");
 var moment = require("moment");;
 var fs = require("fs");
 
-var args = process.argv.slice(2);
-console.log(args)
-// OMDB API with the movie specified
-if (args[0] === "movie-this") {
-    if (args.length == 1) {
-        request(`http://www.omdbapi.com/?t=Mr.nobody&apikey=9d3e0401`, function (error, response, body) {
 
-            if (!error && response.statusCode === 200) {
-                //console.log(body);
-                console.log("Movie's title is: " + JSON.parse(body).Title);
-                console.log("Movie's Year is: " + JSON.parse(body).Year);
-                console.log("Movie's ratings is: " + JSON.parse(body).imdbRating);
-                console.log("Country where movie was produced: " + JSON.parse(body).Country);
-                console.log("Language of the movies: " + JSON.parse(body).Language);
-                console.log("Plot of the movies: " + JSON.parse(body).Plot);
-                console.log("Actors in the movie: " + JSON.parse(body).Actors);
-            }
-        });
-    }
-    else {
+// // var spotify = new Spotify (keys.spotify);
+var spotify = new Spotify({
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+});
 
-        request("http://www.omdbapi.com/?t="+ args[1] +" &apikey=9d3e0401", function (error, response, body) {
+let opr = process.argv[2];
+let term = process.argv[3];
 
-            if (!error && response.statusCode === 200) {
-                //console.log(body);
-                console.log("Movie's title is: " + JSON.parse(body).Title);
-                console.log("Movie's Year is: " + JSON.parse(body).Year);
-                console.log("Movie's ratings is: " + JSON.parse(body).imdbRating);
-                console.log("Country where movie was produced: " + JSON.parse(body).Country);
-                console.log("Language of the movies: " + JSON.parse(body).Language);
-                console.log("Plot of the movies: " + JSON.parse(body).Plot);
-                console.log("Actors in the movie: " + JSON.parse(body).Actors);
-            }
-        });
+main(opr)
+
+function main(switchTerm){
+    switch(switchTerm){
+        case "movie":
+            console.log('movie');
+            OMDBmovie(term);
+            break;
+        case "concert":
+            console.log("consert");
+            concertThis(term);
+            break;
+        case "spotifysign":
+            console.log('spotifysign');
+            spotifyThis(term);
+            break;
+        case "do_this":
+            console.log("do_this");
+            doSomething(term);
+            break;
+        default:break;
     }
 }
 
-if (args[0] == "spotify-this-song") {
-    if (args.length === 1) {
-        var spotify = new Spotify({
-            id: "17e3ab4f921e45489456afdefef6f5e5",
-            secret: "9db5b3ae3a354c078cd69d8f3fd83749 "
-        });
 
-        spotify
-          .search({ type: 'track', query: "The Sign" })
-          .then(function (response) {
-              console.log("Artist(s): " + response.tracks.items[0].album.artists[0].name);
-              console.log("The Song's name: " + response.tracks.items[0].name);
-              console.log("Preview link from Spotify: " + response.tracks.items[0].preview_url);
-              console.log("Album name: " + response.tracks.items[0].album.name);
-          })
-          .catch(function (err) {
-              console.log(err);
-          });
+//case "do_this":
+//     fs.readFile("random.txt", "utf8", function err, data) {
+//         if (err) {
+//             return console.log(err);
+//         }
+//         console.log(data);
+//         var dataArr = data.split(",");
+//         operation = dataArr[0];
+//         term = dataArr[1];
+//         Main()
+//     })
+//     break
+// }
+// }
+
+
+function OMDBmovie(movie) {
+    request ("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy", function (err, response, body) {
+        if (!err && response.statusCode === 200) {
+            let rating = null;
+
+            // console.log(body);
+            let data = JSON.parse(body);
+
+      console.log("The Title of the movie is: " + data.Title);
+      console.log("Year the movie came out: " + data.Year);
+      console.log("Rating of the movie: " + data.imdbRating);    
     }
-    else {
-        var name_arr = args.slice(1);
-        var name = "";
-        for (var i = 0; i < name_arr.length; i++) {
-            name = name + " " + name_arr[i];
-        }
-        var spotify = new Spotify({
-            id: "17e3ab4f921e45489456afdefef6f5e5",
-            secret: "9db5b3ae3a354c078cd69d8f3fd83749 "
-        });
+  })
+}
 
-        spotify
-          .search({ type: 'track', query: name })
-          .then(function (response) {
-              console.log("Artist(s): " + response.tracks.items[0].album.artists[0].name);
-              console.log("The Song's name: " + response.tracks.items[0].name);
-              console.log("Preview link from Spotify: " + response.tracks.items[0].preview_url);
-              console.log("Album name: " + response.tracks.items[0].album.name);
-          })
-          .catch(function (err) {
-              console.log(err);
-          });
+function concertThis (artist) { 
+    request ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function (err, response, body) {
+        if (!err && response.statusCode === 200) {
+            let con = JSON.parse(body);
+            // console.log(JSON.stringify(body, null, 2))
+            for(let i = 0; i < con.length; i++){
+                console.log(con[i]);
+                console.log("venue:" + con[i] .venue.name)
+            }
+        }
+    })
+}
+// // }
+function spotifyThis (song){
+    console.log(song);
+    if(!Spotify){
+        return null;
     }
-}   
-
-if (args[0] === "hey") {
-    fs.readFile("random.txt", "utf8", function (error, data) {
-
-        // If the code experiences any errors it will log the error to the console.
-        if (error) {
-            return console.log(error);
+    spotify.search({type: 'track' , query: song}, function (err, data ){
+        if (err){
+            return console.log('error occurred: ' + err);
         }
-        console.log(data);
-        // Then split it by commas 
-        var dataArr = data.split(",");
-
-        var spotify = new Spotify({
-            id: "17e3ab4f921e45489456afdefef6f5e5",
-            secret: "9db5b3ae3a354c078cd69d8f3fd83749 "
-        });
-
-        spotify
-          .search({ type: 'track', query: dataArr[1] })
-          .then(function (response) {
-              console.log("Artist(s): " + response.tracks.items[0].album.artists[0].name);
-              console.log("The Song's name: " + response.tracks.items[0].name);
-              console.log("Preview link from Spotify: " + response.tracks.items[0].preview_url);
-              console.log("Album name: " + response.tracks.items[0].album.name);
-          })
-          .catch(function (err) {
-              console.log(err);
-          });
-
+        var resp = data;
+    //     console.log('-----')
+    //     console.log("Artist(s): " + data.trackes. item[0].artist[0].name);
+    //     console.log('-----')
+    //     console.log("Song(s): " + data.trackes. item[0].song);
+    //     console.log('-----')
+    //    console.log("name(s): " + data.trackes. item[0].name);
     });
 }
